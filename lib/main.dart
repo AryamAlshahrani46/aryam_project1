45b4bd7ec8b05c15071a07a45b0a420354133b3c
AryamAlshahrani46 <aryammohd1234@gmail.com>
2026-09-09 01:44:44 +0300

    project 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:aryam_project1/screens/home_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main()async {
+Future<void> main()async {
    WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName : ".env");
   await Supabase.initialize(
-    url: "https://lprlgadvlfxgwwuzajrv.supabase.co",
-    publishableKey: "sb_publishable_50ZFBd1llqJC6NTLnBzQZg_nQeWfVpE",
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
+   
   runApp(MainApp());
 } 
 
